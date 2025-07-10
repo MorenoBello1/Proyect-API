941ac7ee168a923b95804ddaa50547490e9aea71
MorenoBello1 <arielmorenobellojeampierre@gmail.com>
2025-07-10 12:49:29 -0500

    cors

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const routing =  require("./router")
+const cors = require('cors');
+
 const app = express();
 const port = 3000;
 
 app.use(express.json())
-app.use('/',routing)
+app.use(cors()); 
 
+app.use('/',routing)
 //middleware si no encuentra ninguna ruta
 app.use((req,res)=>{
   res.status(404).send('TOMA PICHAAA')
