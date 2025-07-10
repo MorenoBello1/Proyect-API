e8b538d0a014b242fbb1ba412415a9f279a7ed56
MorenoBello1 <arielmorenobellojeampierre@gmail.com>
2025-07-10 12:56:00 -0500

    cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const routing =  require("./router")
+const cors = require('cors');
+
 const app = express();
 const port = 3000;
-
+app.use(cors()); 
 app.use(express.json())
 app.use('/',routing)
 
