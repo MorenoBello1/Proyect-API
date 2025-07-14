1490329eaec4d48cf376dc787db23f84fe2afc06
MrFlexEc <itsthebryanyt@gmail.com>
2025-07-14 01:07:59 -0500

    ola

--- db/db.js
+++ db/db.js
@@ -1,16 +1,19 @@
 //hola soy sexo en db mysql? o maria madre de dios ruega por nosotroslos..
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 let conexion = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "proyecto123_",
-  database: "proyect"
+  password: "admin",
+  //password: "proyecto123_",
+  database: "proyect",
 });
 
-conexion.connect((err) =>{
-    if(err)console.log(err);
-  else{console.log("Conectado a mysql");}
+conexion.connect((err) => {
+  if (err) console.log(err);
+  else {
+    console.log("Conectado a mysql");
+  }
 });
 
-module.exports = conexion
+module.exports = conexion;
