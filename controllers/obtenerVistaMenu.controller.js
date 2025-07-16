29e5d3b1506691363160988f75a905ad99b318c9
MorenoBello1 <arielmorenobellojeampierre@gmail.com>
2025-07-15 21:32:46 -0500

    backend correcion recursivo

--- controllers/obtenerVistaMenu.controller.js
+++ controllers/obtenerVistaMenu.controller.js
@@ -9,7 +9,7 @@ exports.vistaobt = (req, res) => {
     return;
   } else {
     const query =
-    `SELECT  u.usuario as usuario, vi.url as url, vi.nombre
+    `SELECT  u.usuario as usuario, vi.url as url, vi.nombre, vi.idpadre, me.idvista
         FROM menu as me
         INNER JOIN users as u ON u.id = me.idusuario
         INNER JOIN vistas as vi ON vi.id = me.idvista
@@ -22,6 +22,20 @@ exports.vistaobt = (req, res) => {
           console.log(err);
           res.status(500).send("Error en la consulta");
           return;
+        }
+
+        if (result.some(e => e.idpadre)) { // si tiene detalle padre 
+
+          let arrayvistahijo = result.filter(e => e.idpadre); //filtramos en nuevo array solo los que tiene padre
+          result.forEach(e => {
+            e.listmenu = []; 
+
+            if (arrayvistahijo.some(x => e.idvista === x.idpadre)) {// si coinciden creamos listmenu y se lo insertamos
+              e.listmenu = arrayvistahijo.filter(x => x.idpadre === e.idvista);
+            }
+          });
+          //esto es para elimnar el extra del que tenia padre por que ese debeeria estar dentro ya de el array 
+          result = result.filter(e=>!e.idpadre || e.idpadre==null)
         }
         res.json(result);
       }
